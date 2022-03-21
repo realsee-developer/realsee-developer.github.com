dd47fc0b679f32f210b300611f4fc5e41ab5bd77
Angry-Sparrow <beqan2016@163.com>
2022-03-21 18:49:25 +0800

    fix: can not find openapi.yaml when build with developer mode

--- src/pages/open/api/index.tsx
+++ src/pages/open/api/index.tsx
@@ -9,10 +9,12 @@ import './custom.css'
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext()
+
+    console.log(siteConfig.baseUrl)
+
   return (
     <Layout title={`如视开放 API${siteConfig.title}`} description="开放共建三维生态，互联大千世界">
-      {/* <API router='hash' basePath='/open/api' apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml" /> */}
-      <BrowserOnly>{() => <API layout="sidebar" hideTryIt={false} router='hash' basePath='' apiDescriptionUrl="/openapi.yaml" />}</BrowserOnly>
+      <BrowserOnly>{() => <API layout="sidebar" hideTryIt={false} router='hash' basePath='' apiDescriptionUrl={`${siteConfig.baseUrl}openapi.yaml`} />}</BrowserOnly>
     </Layout>
   )
 }
