186ab52ed24f4d0ca402012f97f226a89840d5d3
小陀螺 <beqan2016@163.com>
2022-04-01 17:52:36 +0800

    fix: ResponsiveFiveCanvas's z-index (#13)
    fix: Fix display problems caused by z-index

--- src/components/ResponsiveFiveCanvas.tsx
+++ src/components/ResponsiveFiveCanvas.tsx
@@ -9,55 +9,54 @@ import { debounce } from '../utils/debounce'
  */
 
 export function ResponsiveFiveCanvas() {
-  const [size, setSize] = React.useState<{ width: number; height: number }>({ width: 0, height: 0 })
-  const ref = React.useRef<HTMLDivElement>(null)
-  const fixedHeightRef = React.useRef<boolean>(false)
+    const [size, setSize] = React.useState<{ width: number; height: number }>({ width: 0, height: 0 })
+    const ref = React.useRef<HTMLDivElement>(null)
+    const fixedHeightRef = React.useRef<boolean>(false)
 
-  React.useEffect(() => {
-    if (!ref.current) return
+    React.useEffect(() => {
+        if (!ref.current) return
 
-    setSize({
-      width: ref.current.offsetWidth,
-      height: ref.current.offsetHeight,
-    })
+        setSize({
+            width: ref.current.offsetWidth,
+            height: ref.current.offsetHeight,
+        })
 
-    const updateState = debounce(({ width, height }) => {
-      setSize({ width, height })
-      console.log('updateState')
-    })
-    const observer = new ResizeObserver((entries) => {
+        const updateState = debounce(({ width, height }) => {
+            setSize({ width, height })
+            console.log('updateState')
+        })
+        const observer = new ResizeObserver((entries) => {
 
-      const entry = entries[0]
-      const width = entry.contentRect.width
-      const height = entry.contentRect.height
- 
-      updateState({ width, height })
-    })
+            const entry = entries[0]
+            const width = entry.contentRect.width
+            const height = entry.contentRect.height
 
-    observer.observe(ref.current)
+            updateState({ width, height })
+        })
 
-    return () => {
-      if (ref.current) observer.unobserve(ref.current)
-      observer.disconnect()
-    }
-  }, [])
+        observer.observe(ref.current)
 
+        return () => {
+            if (ref.current) observer.unobserve(ref.current)
+            observer.disconnect()
+        }
+    }, [])
 
-  return (
-    <div
-      className="ResponsiveFiveCanvas"
-      ref={ref}
-      style={{
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        zIndex: 1,
-        opacity: size.width === 0 || size.height === 0 ? 0 : 1,
-      }}
-    >
-      <FiveCanvas width={size.width} height={size.height} />
-    </div>
-  )
+    return (
+        <div
+            className="ResponsiveFiveCanvas"
+            ref={ref}
+            style={{
+                width: '100%',
+                height: '100%',
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                // zIndex: 1,
+                opacity: size.width === 0 || size.height === 0 ? 0 : 1,
+            }}
+        >
+            <FiveCanvas width={size.width} height={size.height} />
+        </div>
+    )
 }
