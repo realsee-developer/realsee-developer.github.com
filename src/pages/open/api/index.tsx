ba34e9c32b9fe8d0486957fc7d14bec0a0a6498d
Angry-Sparrow <beqan2016@163.com>
2022-04-08 11:25:40 +0800

    subject: delete console.log

--- src/pages/open/api/index.tsx
+++ src/pages/open/api/index.tsx
@@ -10,8 +10,6 @@ import './custom.css'
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext()
 
-    console.log(siteConfig.baseUrl)
-
   return (
     <Layout title={`如视开放 API${siteConfig.title}`} description="开放共建三维生态，互联大千世界">
       <BrowserOnly>{() => <API layout="sidebar" hideTryIt={false} router='hash' basePath='' apiDescriptionUrl={`${siteConfig.baseUrl}openapi.yaml`} />}</BrowserOnly>
