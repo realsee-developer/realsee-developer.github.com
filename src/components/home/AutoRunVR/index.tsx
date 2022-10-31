e2a8a21e418f15f9e55e4c1671994f20457a1d5a
lichengjie <lichengjie5760@gmail.com>
2022-10-31 16:39:48 +0800

    update:升级docusaurus版本

--- src/components/home/AutoRunVR/index.tsx
+++ src/components/home/AutoRunVR/index.tsx
@@ -1,111 +1,102 @@
-import * as React from 'react'
+import * as React from "react";
 // import { Five, FiveInitArgs, parseWork } from '@realsee/five'
-import { Five, FiveInitArgs, parseWork } from '@realsee/five'
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment'
-import { createFiveProvider, unsafe__useFiveInstance, useFiveEventCallback } from '@realsee/five/react'
+import { Five, FiveInitArgs, parseWork } from "@realsee/five";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+import {
+  createFiveProvider,
+  unsafe__useFiveInstance,
+  useFiveEventCallback,
+} from "@realsee/five/react";
 
-import work from '@realsee/open-works/virtual/81980RNawnbCVaLz0a/work.json'
+import work from "@realsee/open-works/virtual/81980RNawnbCVaLz0a/work.json";
 
-import { ResponsiveFiveCanvas } from '../../ResponsiveFiveCanvas'
-import { Rotation } from '@realsee/dnalogel/plugins/CameraMovementPlugin/typing'
+import { ResponsiveFiveCanvas } from "../../ResponsiveFiveCanvas";
+import { Rotation } from "@realsee/dnalogel/plugins/CameraMovementPlugin/typing";
+import { CameraMovementPlugin } from "@realsee/dnalogel/plugins/CameraMovementPlugin";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
-export function createAutoRunVR() {
+const waiting = (time: number) =>
+  new Promise((resolve) => setTimeout(() => resolve(true), time));
 
+const AutoRun = () => {
+  const five = unsafe__useFiveInstance();
+
+  const animation = React.useCallback(async () => {
+    const cameraMovementPlugin = five.plugins.cameraMovementPlugin;
+    await cameraMovementPlugin.rotate(
+      {
+        fov: 120,
+        latitude: 0.12900782075671083,
+        longitude: 6.230574572010345,
+        rotation: Rotation.Anticlockwise,
+      },
+      15000
+    );
+    // 2s
+    await five.changeMode(Five.Mode.Floorplan, {}, 2000);
+    // 2s
+    await waiting(2000);
+    // 5s
+    await five.updateCamera(
+      {
+        fov: 80,
+        latitude: 0.9376970139721618,
+        longitude: 2.7173023248098125,
+      },
+      5000
+    );
+    // 2s
+    await waiting(2000);
+
+    // 2s
+    await five.changeMode(
+      Five.Mode.Panorama,
+      {
+        fov: 90,
+        latitude: 0,
+        longitude: 1.4774911460398048,
+      },
+      2000
+    );
+
+    await waiting(2000);
+    return await animation();
+  }, []);
+
+  React.useEffect(() => {
+    five.ready().then(async () => await animation());
+  }, [five]);
+
+  return <></>;
+};
+
+const AutoRunVR = () => {
   const defaultInitArgs: FiveInitArgs = {
-    backgroundColor: 0xff0000,
     imageOptions: { size: 256 },
     textureOptions: { size: 64 },
-    onlyRenderIfNeeds: true,
-    antialias: false,
-    model: {},
-  }
+    plugins: [[CameraMovementPlugin, "cameraMovementPlugin"]],
+  };
 
-  const FiveProvider = createFiveProvider(defaultInitArgs)
+  const FiveProvider = createFiveProvider(defaultInitArgs);
 
-  const waiting = (time: number) =>
-    new Promise((resolve) => {
-      setTimeout(() => resolve(true), time)
-    })
+  return (
+    <FiveProvider
+      initialWork={parseWork(work)}
+      initialState={{
+        panoIndex: 7,
+        fov: 90,
+        mode: "Panorama",
+      }}
+      ref={(ref) => {
+        if (ExecutionEnvironment.canUseDOM) {
+          Object.assign(window, { $five: ref?.five });
+        }
+      }}
+    >
+      <ResponsiveFiveCanvas />
+      <AutoRun />
+    </FiveProvider>
+  );
+};
 
-  function AutoRun() {
-    const five = unsafe__useFiveInstance()
-
-    useFiveEventCallback('modelLoaded', () => {
-      if (!ExecutionEnvironment.canUseDOM) {
-        return
-      }
-      const CameraMovementPlugin = require('@realsee/dnalogel/plugins/CameraMovementPlugin').default
-      const cameraMovementPlugin = CameraMovementPlugin(five, {})
-
-      const callback = async () => {
-        // 15s
-        await cameraMovementPlugin.rotate(
-          {
-            fov: 120,
-            latitude: 0.12900782075671083,
-            longitude: 6.230574572010345,
-            rotation: Rotation.Anticlockwise,
-          },
-          15000
-        )
-        // 2s
-        await five.changeMode(Five.Mode.Floorplan, {}, 2000)
-        // 2s
-        await waiting(2000)
-        // 5s
-        await five.updateCamera(
-          {
-            fov: 80,
-            latitude: 0.9376970139721618,
-            longitude: 2.7173023248098125,
-          },
-          5000
-        )
-        // 2s
-        await waiting(2000)
-
-        // 2s
-        await five.changeMode(
-          Five.Mode.Panorama,
-          {
-            fov: 90,
-            latitude: 0,
-            longitude: 1.4774911460398048,
-          },
-          2000
-        )
-
-        await waiting(2000)
-        callback()
-      }
-      callback()
-
-      return () => {}
-    })
-    return <></>
-  }
-
-  return function AutoRunVR() {
-    if (!ExecutionEnvironment.canUseDOM) {
-      return <></>
-    }
-    return (
-      <FiveProvider
-        initialWork={parseWork(work)}
-        initialState={{
-          panoIndex: 7,
-          fov: 90,
-          mode: 'Panorama',
-        }}
-        ref={(ref) => {
-          if (ExecutionEnvironment.canUseDOM) {
-            Object.assign(window, { $five: ref?.five })
-          }
-        }}
-      >
-        <ResponsiveFiveCanvas />
-        <AutoRun />
-      </FiveProvider>
-    )
-  }
-}
+export default () => <BrowserOnly>{() => <AutoRunVR></AutoRunVR>}</BrowserOnly>;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,62 +1,73 @@
-import React from 'react'
-import BrowserOnly from '@docusaurus/BrowserOnly'
-import Layout from '@theme/Layout'
-import Link from '@docusaurus/Link'
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import HomepageFeatures from '../components/HomepageFeatures'
-import { debounce } from '@mui/material'
-import { createAutoRunVR } from '../components/home/AutoRunVR'
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment'
-import './index.css'
-
-const AutoRunVR = createAutoRunVR()
+import React from "react";
+import BrowserOnly from "@docusaurus/BrowserOnly";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import HomepageFeatures from "../components/HomepageFeatures";
+import { debounce } from "@mui/material";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+import "./index.css";
 
 const getWindowInnerHeight = () => {
   if (ExecutionEnvironment.canUseDOM) {
-    return window.innerHeight
+    return window.innerHeight;
   }
 
-  return 720
-}
+  return 720;
+};
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext()
-  const [height, setHeight] = React.useState(getWindowInnerHeight() - 60)
+  const { siteConfig } = useDocusaurusContext();
+  const [height, setHeight] = React.useState(getWindowInnerHeight() - 60);
 
   React.useEffect(() => {
-    if (!ExecutionEnvironment.canUseDOM) return
+    if (!ExecutionEnvironment.canUseDOM) return;
     const listener = debounce(() => {
-      setHeight(window.innerHeight - 60)
-    })
+      setHeight(window.innerHeight - 60);
+    });
 
-    window.addEventListener('resize', listener)
+    window.addEventListener("resize", listener);
 
     return () => {
-      window.removeEventListener('resize', listener)
-    }
-  }, [])
+      window.removeEventListener("resize", listener);
+    };
+  }, []);
 
   return (
-    <header className="hero hero--primary cover__header" style={{ height: height + 'px' }}>
-      <AutoRunVR />
+    <header
+      className="hero hero--primary cover__header"
+      style={{ height: height + "px" }}
+    >
+      <BrowserOnly>
+        {() => {
+          const AutoRunVR = require("../components/home/AutoRunVR").default;
+          return <AutoRunVR />;
+        }}
+      </BrowserOnly>
       <div className="cover__header_mask"></div>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div>
-          <Link className="button button--secondary button--lg button--entry" to="/docs/front/3d-space/intro">
+          <Link
+            className="button button--secondary button--lg button--entry"
+            to="/docs/front/3d-space/intro"
+          >
             进入阅读
           </Link>
         </div>
       </div>
     </header>
-  )
+  );
 }
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext()
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`${siteConfig.title}`} description="开放共建三维生态，互联大千世界">
+    <Layout
+      title={`${siteConfig.title}`}
+      description="开放共建三维生态，互联大千世界"
+    >
       <BrowserOnly>
         {() => (
           <>
@@ -67,7 +78,6 @@ export default function Home(): JSX.Element {
           </>
         )}
       </BrowserOnly>
-
     </Layout>
-  )
+  );
 }
