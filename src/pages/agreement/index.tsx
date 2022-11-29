a580496a08d9c0e954945d6cd7a818a6a4c38089
lichengjie <lichengjie5760@gmail.com>
2022-11-29 18:14:42 +0800

    feat:法务协议

--- src/pages/agreement/index.tsx
+++ src/pages/agreement/index.tsx
@@ -3,19 +3,20 @@ import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import "../index.css";
 import style from "./style.module.css";
-import AgreementMD from "./_agreement.mdx";
 import Translate, { translate } from "@docusaurus/Translate";
+import AgreementMD from "./_agreement.mdx";
+import AgreementMDEn from "./_agreement-en.mdx";
 
 export default function Agreement(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext()
-
+  const { siteConfig, i18n } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
       description={<Translate>开放共建三维生态，互联大千世界</Translate>}
     >
       <div className={style.Page}>
-        <AgreementMD />
+        {i18n.currentLocale === "zh-CN" && <AgreementMD />}
+        {i18n.currentLocale === "en" && <AgreementMDEn />}
       </div>
     </Layout>
   );
